669e93ad987fb4afa9d96e8783bc8fadd07bbfd7
vagrant <vagrant@localhost.localdomain>
2020-10-03 12:58:15 +0000

    [Add]投稿機能destroy以外の投稿機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,30 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:edit, :update, :destroy, :show]
+
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -18,5 +34,16 @@ class PostsController < ApplicationController
   end
 
   def index
+    @posts = Post.all
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:user_id, :title, :body, :post_image, :tag_list)
   end
 end
